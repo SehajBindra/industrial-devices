65ff602a332ce67f7867d4e3e5df3f46a2724426
SehajBindra <sehajbindra1234@gmail.com>
2026-06-12 19:15:32 +0530

    Update BlogSection styling for improved readability and branding
    - Increased maximum width of the blog section text for better layout.
    - Updated text to "Latest Insights & Updates from" for enhanced clarity and engagement.
    - Adjusted text styling for improved visual appeal.

--- components/blog/blog-section.tsx
+++ components/blog/blog-section.tsx
@@ -8,9 +8,9 @@ export function BlogSection() {
       <div>
         <div className="flex min-h-full w-full items-center justify-center">
           <section className="mx-auto w-full max-w-7xl px-4 py-16 md:px-8 md:py-20">
-            <p className="my-6 max-w-2xl text-2xl  tracking-tight text-balance text-neutral-500 md:text-4xl lg:text-5xl dark:text-neutral-200">
-              Latest news and updates from <br />
-              <span className="bg-linear-to-b from-primary to-primary bg-clip-text text-transparent text-shadow-md">
+            <p className="my-6 max-w-3xl text-2xl  tracking-normal  text-balance text-neutral-500 md:text-4xl lg:text-5xl dark:text-neutral-200">
+              Latest Insights & Updates from <br />
+              <span className="text-primary mt-1  text-shadow-md">
                 Industrial Devices
               </span>
             </p>
